66edfa8566fc048f6f408308358d3b81182ed2e8
abdulmaajidaga <amaajidking@gmail.com>
2024-03-29 16:40:14 +0400

    pykin pub modified

--- b38ro_ws/src/joints/joints/pykinpub.py
+++ b38ro_ws/src/joints/joints/pykinpub.py
@@ -1,9 +1,15 @@
-# Import necessary libraries for ROS 2
-import rclpy  # Import the ROS 2 Python library
-from rclpy.node import Node  # Import the Node class for creating ROS nodes
-from std_msgs.msg import String, Float32MultiArray  # Import message types for communication
+import rclpy
+from rclpy.node import Node
+from rclpy.time import Duration
+from geometry_msgs.msg import Pose
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
 from pykin.robots.single_arm import SingleArm  # Import SingleArm robot class from Pykin
 from pykin.kinematics.transform import Transform  # Import Transform class from Pykin for defining transformations
+import numpy as np
+
+jointNames = ["joint_1", "joint_2", "joint_3", "joint_4", "joint_5", "joint_6"]
+
 
 # Define a class for our ROS node
 class MinimalPublisher(Node):
@@ -11,16 +17,13 @@ class MinimalPublisher(Node):
     # Constructor method
     def __init__(self):
         super().__init__("joints")  # Initialize the node with name "joints"
-        
-        # Create a publisher for target joint angles
+
+        # Create a publisher for target angles
         self.target_angles = self.create_publisher(
-            Float32MultiArray, "target_angles", 10
+            JointTrajectory, "joint_trajectory_controller/joint_trajectory", 10
         )
-        
-        # Create a subscription to receive Cartesian poses
-        self.create_subscription(
-            Float32MultiArray, "cart_pose", self.move_to_angles, 10
-        )
+        # Create a subscription to recieve cartesian target positions
+        self.create_subscription(Pose, "cart_pose", self.move_to_angles, 10)
 
         # Define the file path to the URDF file
         file_path = '../assets/urdf/KR7108-URDF/KR7108-URDF.urdf'
@@ -30,7 +33,7 @@ class MinimalPublisher(Node):
         self.robot.setup_link_name("BASE", "DUMMY")
 
     # Method to calculate joint angles and publish target angles
-    def move_to_angles(self, msg: Float32MultiArray):
+    def move_to_angles(self, msg: Pose):
 
         # Initial joint angles
         init_thetas = [0, 0, 0, 0, 0, 0]
@@ -42,32 +45,34 @@ class MinimalPublisher(Node):
 
         # Calculate forward kinematics to get the initial end-effector pose
         self.robot.forward_kin(init_thetas)
-        
-        # Calculate inverse kinematics for the received Cartesian pose
-        angles = self.robot.inverse_kin(init_thetas, msg.data)
-    
+
+        cart_pose = np.concatenate((msg.position, msg.orientation))
+        # Calculate inverse kinematics for the recived cartesian positions
+        angles = self.robot.inverse_kinematics(
+            init_thetas, cart_pose
+        )
+
         # Create a new message for publishing
-        msg = Float32MultiArray() 
-        
-        # Set the data field of the message to the calculated joint angles
-        msg.data = angles.tolist()
-        
-        # Print the calculated joint angles
-        print(msg.data)
+        traj = JointTrajectory()
+        traj.joint_names = jointNames
+        point = JointTrajectoryPoint()
+        point.positions = angles.tolist()
+        point.time_from_start = Duration(seconds=10).to_msg()
+        traj.points.append(point)
 
-        # Publish the calculated joint angles
-        self.target_angles.publish(msg)
+        print(angles, len(jointNames), traj.points[0])
+        print(traj)
+        self.target_angles.publish(traj)
 
 
-# Main function to initialize the node and start the event loop
+# Boilerplate ROS code for making a node
 def main():
-    rclpy.init()  # Initialize the ROS 2 client library
-    minimal_publisher = MinimalPublisher()  # Create an instance of MinimalPublisher
+    rclpy.init()
+    minimal_publisher = MinimalPublisher()
     rclpy.spin(minimal_publisher)
-    minimal_publisher.destroy_node()  # Explicitly destroy the node
-    rclpy.shutdown()  # Shutdown the ROS 2 client library
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
 
 
-# Entry point of the script
 if __name__ == "__main__":
-    main()  # Call the main function to start the node
+    main()
