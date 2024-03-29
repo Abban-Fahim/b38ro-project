c7383832157c040f0f5c533b379792f1a9309111
Laith0 <itslaithmohamed@gmail.com>
2024-03-29 17:38:56 +0400

    small fix to tttai

--- b38ro_ws/src/joints/ROSMAIN/libs/tttai.py
+++ b38ro_ws/src/joints/ROSMAIN/libs/tttai.py
@@ -42,7 +42,7 @@ def tttai(board):
                 move = i
 
     return move
-def haswin(player, board):
+def win(player, board):
     win_conditions = [(0, 1, 2), (3, 4, 5), (6, 7, 8),(0, 3, 6), (1, 4, 7), (2, 5, 8),(0, 4, 8), (2, 4, 6)]
     return any(all(board[i] == player for i in condition) for condition in win_conditions)
 
