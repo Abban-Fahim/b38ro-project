ff7cbf733735a4768d5e880909dcf250fa5e6847
Laith0 <itslaithmohamed@gmail.com>
2024-04-01 17:35:44 +0400

    essage=small add ,working on function to return array of board positions given two corners

---
+++ b38ro_ws/src/joints/ROSMAIN/libs/firstmove.py
@@ -0,0 +1,13 @@
+#imports 
+
+
+
+#functions
+
+def ai_first():
+	#temp implementation via input ,possible to have as part of a web UI 
+	if input("Press 1 for Human to move first")==1:
+		return True
+	else:
+		return False
+		
---
+++ b38ro_ws/src/joints/ROSMAIN/libs/graphtests.py
@@ -0,0 +1,34 @@
+#testing equations ect to try and derrive method +equations to get cartesian positions given 
+# two opposite points in board ,ie;
+# x |   |     given the x'es cartesian positions (where x is where the robot would place)\
+#   |   |     
+#   |   | x   deduce each of the 9 places cartesian positions 
+
+
+#  (0,0) | (1,0) | (2,0)
+# 
+# 
+
+import math as m
+
+
+def TwoPointToEquation(p1,p2):
+    a = (p1[1]-p2[1])
+    b = (p2[0]-p1[0])
+    c = (((p2[1]-p1[1])*p2[0])-((p2[0]-p1[0])*p2[1]))
+    
+    return [a,b,c]
+
+def TwoPointDist(p1,p2):
+    return m.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
+  
+def LinePerp(eq,p1):
+    return [eq[1],-eq[0],(-eq[1]*p1[0]+eq[0]*p1[1])]
+
+def Intersect(eq1,eq2):
+    return [((eq1[1]*eq2[1]-eq1[2]*eq2[0])/(eq1[0]*eq2[1]-eq1[1]*eq2[0])),((eq1[2]*eq2[0]-eq1[0]*eq2[2])/(eq1[0]*eq2[1]-eq[1]*eq[0]))]
+
+def PointbelowLine(eq1,p1,d):
+    return [(p1[0]-eq1[1]/m.sqrt(eq1[0]**2+(eq1[1]**2)*d)),(p1[1]+eq1[0]/m.sqrt(eq1[0]**2+(eq1[1]**2)*d))]
+    
+
