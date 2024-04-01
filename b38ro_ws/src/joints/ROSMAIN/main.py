7ecf1faf3547e5b240fd93a50fac33fd412270d3
Laith0 <itslaithmohamed@gmail.com>
2024-04-01 17:01:01 +0400

    essage=small add ,working on function to return array of board positions given two corners

--- b38ro_ws/src/joints/ROSMAIN/main.py
+++ b38ro_ws/src/joints/ROSMAIN/main.py
@@ -7,11 +7,20 @@ import firstmove.py as fm
 #initalisation 
 r.setup()
 m.def_pos()#move robot arm to default position 
+p_s=m.det_bord_cart()#calibrate the position of each sqaure 
 b_s=[0,0,0,0,0,0,0,0,0]
 
 # determine who goes first 
-if ai(fm.h_first())
-	md = w.wait()
+if (fm.h_first())
+	md = w.wait()#wait for user to make move 
+	#current implement also has user input move they made 
+	b_s[md]=1
+	#if we go with comp vision we'll have 3 nodes ,one for move detection ,one for board position and one for main logic 
+
+
+
+
+	
 	
 	
 
