be87eeb44cd3c8b49d24ac60462636755d34325a
Laith0 <itslaithmohamed@gmail.com>
2024-04-02 14:53:02 +0400

    essage=finished alg for tiktaktoe positions

--- b38ro_ws/src/joints/ROSMAIN/libs/graphtests.py
+++ b38ro_ws/src/joints/ROSMAIN/libs/graphtests.py
@@ -12,27 +12,56 @@
 import math as m
 
 
-def TwoPointToEquation(p1,p2):
+def tpte(p1,p2):
     a = (p1[1]-p2[1])
     b = (p2[0]-p1[0])
     c = (((p2[1]-p1[1])*p2[0])-((p2[0]-p1[0])*p2[1]))
     
     return [a,b,c]
 
-def TwoPointDist(p1,p2):
+def tpd(p1,p2):
     return m.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
   
-def LinePerp(eq,p1):
+def pl(eq,p1):
     return [eq[1],-eq[0],(-eq[1]*p1[0]+eq[0]*p1[1])]
 
-def Intersect(eq1,eq2):
-    return [((eq1[1]*eq2[1]-eq1[2]*eq2[0])/(eq1[0]*eq2[1]-eq1[1]*eq2[0])),((eq1[2]*eq2[0]-eq1[0]*eq2[2])/(eq1[0]*eq2[1]-eq[1]*eq[0]))]
+def sec(eq1,eq2):
+    return [((eq1[1]*eq2[2]-eq1[2]*eq2[1])/(eq1[0]*eq2[1]-eq1[1]*eq2[0])),((eq1[2]*eq2[0]-eq1[0]*eq2[2])/(eq1[0]*eq2[1]-eq1[1]*eq2[0]))]
 
-def PointbelowLine(eq1,p1,d):
-    return [(p1[0]-eq1[1]/m.sqrt(eq1[0]**2+(eq1[1]**2)*d)),(p1[1]+eq1[0]/m.sqrt(eq1[0]**2+(eq1[1]**2)*d))]
+ 
+def lbl(eq1,p1,d):
+    return [eq1[0],eq1[1],(d*m.sqrt(eq1[0]**2+eq1[1]**2)-eq1[0]*p1[0]-eq1[1]*p1[1])]
+    
+def ris(p1,p2):
+    d = tpd(p1,p2)
+    
+    l1 = tpte(p1,p2)#
+    l1p = pl(l1,p1)#tem
+    lp1 = lbl(l1p,p1,-d/4)#
+    lp2 = lbl(l1p,p1,-d/2)#
+    lp3 = lbl(l1p,p1,-3*d/4)
+    l2 = lbl(l1,p1,d/2)
+    l3 = lbl(l1,p1,d/4)
+    l4 = lbl(l1,p1,-d/2)
+    l5 = lbl(l1,p1,-d/4)
+
+    
+    q0 = p1
+    q1 = sec(lp1,l5)
+    q2 = sec(lp2,l4)
     
     
+    q3 = sec(lp1,l3)
+    q4 = sec(l1,lp2)
+    q5 = sec(lp3,l5)
     
+    q6 = sec(lp2,l2)
+    q7 = sec(lp3,l3)
+    q8 = p2
+    
+
+    
+    return [q0,q1,q2,q3,q4,q5,q6,q7,q8]
 # derrived method :
 # need to make 8 lines ,line joining the two points ,4 parralell and 3 perpendicular 
 # the 4 parralell lines ,two to the left and 2 right .
@@ -41,3 +70,5 @@ def PointbelowLine(eq1,p1,d):
 # and one on the midpoint bitween them 
 
 #check desmos graph for more detail https://www.desmos.com/calculator/lmcito3cnm
+
+
